331934bd8a92a9d838224cadd50507a5b461224c
Thanh Ha <thanh.ha@s3corp.com.vn>
2025-05-16 13:28:08 +0700

    add credential

--- be/api/middleware/auth.go
+++ be/api/middleware/auth.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"BE_Manage_device/config"
 	"BE_Manage_device/constant"
 	"BE_Manage_device/pkg"
 	"net/http"
@@ -53,6 +54,14 @@ func AuthMiddleware(secretKey string) gin.HandlerFunc {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		origin := c.Request.Header.Get("Origin")
+
+		// Chỉ cho phép origin hợp lệ (bạn có thể kiểm tra whitelist nếu cần)
+		if origin == config.BASE_URL_FRONTEND {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
--- be/internal/domain/entity/session.go
+++ be/internal/domain/entity/session.go
@@ -5,8 +5,8 @@ import "time"
 type UsersSesions struct {
 	Id           int64     `gorm:"primaryKey;autoIncrement" json:"id"`
 	UserId       int64     `gorm:"not null"`
-	RefreshToken string    `gorm:"type:text;not null"`
-	AccessToken  string    `gorm:"type:text;not null"`
+	RefreshToken string    `gorm:"type:text"`
+	AccessToken  string    `gorm:"type:text"`
 	CreatedAt    time.Time `gorm:"not null" json:"created_at"`
 	ExpiresAt    time.Time `gorm:"not null" json:"expires_at"`
 	IsRevoked    bool      `gorm:"default:false" json:"role_id"`
